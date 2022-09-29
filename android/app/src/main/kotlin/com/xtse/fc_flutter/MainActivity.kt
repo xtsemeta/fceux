f4a881acaa1edd5fb2408b5fc879fa25df93e0ce
xtsemeta <xtsemeta@gmail.com>
2022-09-29 09:40:33 +0800

    init

---
+++ android/app/src/main/kotlin/com/xtse/fc_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xtse.fc_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/fc/fc.dart
@@ -0,0 +1,16 @@
+
+
+class FC{
+static const JOY_A         = 0x01;
+static const JOY_B         = 0x02;
+static const JOY_SELECT    = 0x04;
+static const JOY_START     = 0x08;
+static const JOY_UP        = 0x10;
+static const JOY_DOWN      = 0x20;
+static const JOY_LEFT      = 0x40;
+static const JOY_RIGHT     = 0x80;
+
+
+void free(String filename){}
+
+}
